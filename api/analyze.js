13d8cef710d57bb60c28e62a0081cfb93d3f9364
kbohare95-droid <kbohare95@gmail.com>
2026-09-25 11:38:30 +0530

    Update analyze.js

--- api/analyze.js
+++ api/analyze.js
@@ -462,7 +462,7 @@ Return ONLY the JSON object.
 `;
 
     const response = await fetch(
-  "https://generativelanguage.googleapis.com/v1beta/models/gemini-3.8-flash:generateContent?key=" +
+  "https://generativelanguage.googleapis.com/v1beta/models/gemini-3.5-flash:generateContent?key=" +
     process.env.GEMINI_API_KEY,
   {
     method: "POST",
